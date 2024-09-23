02221883f292045ff587d5ae7f418ec922d5eef8
Kkonmo <steven5797@naver.com>
2024-09-23 15:55:19 +0900

    [Add] 물고기 애니메이션 속도조절
    ↑키는 속도 증가, ↓키는 속도 감소
    space바는 애니메이션 on off

--- Assets/FishScript.cs
+++ Assets/FishScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FishScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 0;
     void Start()
     {
         
@@ -13,6 +13,20 @@ public class FishScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Animator>().speed += Time.deltaTime * 0.5f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Animator>().speed -= Time.deltaTime * 0.5f;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            if (GetComponent<Animator>().GetBool("isSwimming")) GetComponent<Animator>().SetBool("isSwimming", false);
+            else GetComponent<Animator>().SetBool("isSwimming", true);
+
+            GetComponent<Animator>().speed = 1;
+        }
     }
 }
