31545b1b51d01fe1807b10a6429fe2b91e90a345
yuyerin <annie7373@naver.com>
2024-10-18 00:42:35 +0900

    [add] 배경 3d오브젝트 변경

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -9,6 +9,8 @@ public class MapManager : MonoBehaviour
     public GameObject skyPanel;
     public GameObject groundPanel;
 
+    public GameObject uiCanvas;
+
     private VideoPlayer videoPlayer;
 
     public void OnSeaButtonClick()
@@ -32,6 +34,9 @@ public class MapManager : MonoBehaviour
     private void PlayVideo(GameObject panel)
     {
         videoPlayer = panel.GetComponent<VideoPlayer>(); // 패널에 있는 VideoPlayer 컴포넌트 가져옴
+
+        uiCanvas.SetActive(false); // UI 비활성화
+
         videoPlayer.Play();                              // 영상 재생
         videoPlayer.loopPointReached += EndReached;      // 영상 끝나는 이벤트에 함수 등록
     }
@@ -39,5 +44,7 @@ public class MapManager : MonoBehaviour
     private void EndReached(VideoPlayer vp)
     {
         vp.gameObject.SetActive(false);  // 패널 비활성화
+
+        uiCanvas.SetActive(true); //UI 다시 활성화
     }
 }
